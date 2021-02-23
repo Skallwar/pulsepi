dad7a8903db705d346a96c7741dd37ab34439a27
Esteban Blanc <esteban.blanc@lse.epita.fr>
2021-02-23 16:58:25 +0100

    Add auto restart

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ fi
 systemctl --global disable pulseaudio.service pulseaudio.socket
 
 # Copy config files
-cp system.pa /etc/pulse/system.pa
-cp pulseaudio.service /etc/systemd/system/pulseaudio.service
+ln -si $PWD/system.pa /etc/pulse/system.pa
+ln -si $PWD/pulseaudio.service /etc/systemd/system/pulseaudio.service
 
 # Enable system wide pulseaudio
 systemctl enable pulseaudio.service
